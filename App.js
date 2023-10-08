1cc29d72c62514335d0f6241ffdbc77eed53bf25
your_username <your_email@example.com>
2023-10-08 15:57:57 +0800

    XuanYou2ndtime

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import MainScreen from './MainScreen';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
-import Page4 from './Page4';
 import Page5 from './Page5';
 import Page6 from './Page6';
+import Safety_plan from './Page4';
 
 const Stack = createStackNavigator();
 
@@ -19,7 +19,7 @@ export default function App() {
         <Stack.Screen name="Page1" component={Page1} />
         <Stack.Screen name="Page2" component={Page2} />
         <Stack.Screen name="Page3" component={Page3} />
-        <Stack.Screen name="Page4" component={Page4} />
+        <Stack.Screen name="Page4" component={Safety_plan} />
         <Stack.Screen name="Page5" component={Page5} />
         <Stack.Screen name="Page6" component={Page6} />
       </Stack.Navigator>
--- MainScreen.js
+++ MainScreen.js
@@ -88,86 +88,149 @@ export default function MainScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.rowStyle}>
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick1}>
-          <Image
-            source={require('./assets/SOS_pic.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Emergency</Text>
-        </TouchableOpacity>
+      <View style={styles.gridContainer}>
+      <View style={styles.imageContainer}>
+  <Image
+    source={require('./assets/safespace1.png')}
+    style={{ width: 300, height: 270 }}
+    resizeMode="contain"
+  />
+  <Image
+    source={require('./assets/heartspeak1.png')}
+    style={{ width: 50, height: 50, position: 'absolute', right: -15, bottom:10, }}
+    resizeMode="contain"
+  />
+    <Image
+    source={require('./assets/heartspeak1.png')}
+    style={{ width: 60, height: 60, position: 'absolute', left: 40, bottom:-25, }}
+    resizeMode="contain"
+  />
+     <Image
+    source={require('./assets/heartspeak1.png')}
+    style={{ width: 30, height: 30, position: 'absolute', left: 40, bottom: -400, }}
+    resizeMode="contain"
+  />
+    <Image
+    source={require('./assets/cute(2).png')}
+    style={{ width: 50, height: 60, position: 'absolute', left: -30, top: 25, }}
+    resizeMode="contain"
+  />
+      <Image
+    source={require('./assets/cute(2).png')}
+    style={{ width: 100, height: 100, position: 'absolute', right: -30, top: 630, }}
+    resizeMode="contain"
+  />
+  <Image
+    source={require('./assets/heartspeak1.png')}
+    style={{ width: 40, height: 40, position: 'absolute', left: -30, bottom:-200, }}
+    resizeMode="contain"
+  />
+</View>
+        {/* Row 1 */}
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick1}>
+            <Image
+              source={require('./assets/SOS_pic.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Emergency</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick2}>
-          <Image
-            source={require('./assets/Contact_professional.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Contact Professional</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick2}>
+            <Image
+              source={require('./assets/Contact_professional.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Contact Professional</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Row 2 */}
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick3}>
+            <Image
+              source={require('./assets/document_abuse.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Document Abuse</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick4}>
+            <Image
+              source={require('./assets/safety_plan.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Safety Plan</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Row 3 */}
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick5}>
+            <Image
+              source={require('./assets/heartbroken.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Self Care</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonRectangle}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick6}>
+            <Image
+              source={require('./assets/warning_sign.png')}
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+            />
+            <Text style={styles.buttonText}>Warning Sign</Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
-      <View style={styles.rowStyle}>
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick3}>
-          <Image
-            source={require('./assets/document_abuse.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Document Abuse</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick4}>
-          <Image
-            source={require('./assets/safety_plan.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Safety Plan</Text>
-        </TouchableOpacity>
-      </View>
-   
-      <View style={styles.rowStyle}>
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick5}>
-          <Image
-            source={require('./assets/heartbroken.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Self Care</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleEmergencyClick6}>
-          <Image
-            source={require('./assets/warning_sign.png')}
-            style={{ width: 60, height: 60 }}
-            resizeMode="contain"
-          />
-          <Text style={styles.buttonText}>Warning Sign</Text>
-        </TouchableOpacity>
+      {/* Separate View for heartspeak.jpg */}
+      <View>
+        <Image
+          source={require('./assets/map.png')}
+          style={{ width: 60, height: 60 }}
+          resizeMode="contain"
+        />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    padding: 40,
+  },
+
+  buttonRectangle: {
+    flexBasis: '35%',
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 20,
+    borderWidth: 1, // Add border width
+    borderColor: '#dcdcdc', // Add border color
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  rowStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  buttonText: {
-    fontSize: 16,
-    marginTop: 10,
+    backgroundColor: '#f1dcf7', // Set the background color here
+    padding: 10,
   },
 });
