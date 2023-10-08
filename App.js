0321ddee3cfacaabe8141d48a6d2c1aa68466947
your_username <your_email@example.com>
2023-10-09 03:16:11 +0800

    XY3amstuff

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './MainScreen';
-import Page1 from './Page1';
+import Emergency from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
 import Page5 from './Page5';
 import Page6 from './Page6';
+import Page7 from './Page7';
+import Page8 from './Page8';
 import Safety_plan from './Safety_plan';
 
 const Stack = createStackNavigator();
@@ -16,12 +18,14 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Main">
         <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="Page1" component={Page1} />
+        <Stack.Screen name="Page1" component={Emergency} />
         <Stack.Screen name="Page2" component={Page2} />
         <Stack.Screen name="Page3" component={Page3} />
         <Stack.Screen name="Safety_plan" component={Safety_plan} />
         <Stack.Screen name="Page5" component={Page5} />
         <Stack.Screen name="Page6" component={Page6} />
+        <Stack.Screen name="Page7" component={Page7} />
+        <Stack.Screen name="Page8" component={Page8} />
       </Stack.Navigator>
     </NavigationContainer>
   );
